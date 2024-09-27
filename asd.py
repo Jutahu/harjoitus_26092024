80c968ce4a9d9183cd4a5036d0f2936ff8c2f079
juhay-git <juha.yli-hemminki@seamk.fi>
2024-09-27 10:14:55 +0300

    Lisätty tulostuksia

--- asd.py
+++ asd.py
@@ -1,2 +1,13 @@
 '''juuuh'''
 print("moi juha xdd")
+
+def moro(n):
+    if n == 1:
+        print("")
+        return 1
+    else:
+        print("moi!", end=" ")
+        return moro(n - 1)
+
+
+moro(3)
