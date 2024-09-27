777ff98fceee76447b519c53494f914a14af523e
Jutahu <jukka.huhtakallio@gmail.com>
2024-09-27 10:43:08 +0300

    looool

--- asd.py
+++ asd.py
@@ -8,6 +8,14 @@ def moro(n):
     else:
         print("moi!", end=" ")
         return moro(n - 1)
+def xdd(n2):
+    if n2 == 1:
+        print("")
+        return 1
+    else:
+        print("x"+"d"*n2, end=" ")
+        return xdd(n2 - 1)
 
 
 moro(3)
+xdd(6)
